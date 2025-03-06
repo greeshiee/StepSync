ea1b973703a1c0d62bc1286bc56657f624c82ca5
masorol <miasorola.yoshida@gmail.com>
2025-03-05 20:22:07 -0600

    initial VideoUpload page

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Homepage from './Homepage';
-import Dashboard from './Dashboard';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./Homepage";
+import Dashboard from "./Dashboard";
+import VideoUpload from "./VideoUpload";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/upload" element={<VideoUpload />} />
       </Routes>
     </Router>
   </React.StrictMode>
