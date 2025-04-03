a49ed2952345237cf9ed77520da31d660b309f42
masorol <miasorola.yoshida@gmail.com>
2025-04-02 21:07:41 -0500

    adding functionality to button

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import Navbar from './Navbar';
+import React from "react";
+import Navbar from "./Navbar";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center h-screen p-5 bg-gray-50">
       <Navbar />
@@ -11,9 +13,12 @@ const Dashboard = () => {
       </h1>
 
       <div className="w-3/4 h-[100px] bg-white border-2 border-black rounded-sm mt-8 flex flex-col items-start justify-center pl-5 shadow-md">
-        <p className="text-[18px] text-black cursor-pointer hover:text-blue-600 transition">
+        <button
+          className="text-[18px] text-black cursor-pointer hover:text-blue-600 transition"
+          onClick={() => navigate("/upload")}
+        >
           + Analyze new dance
-        </p>
+        </button>
       </div>
     </div>
   );
